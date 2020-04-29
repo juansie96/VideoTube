4fbdc97ff045ee175705c0d13e86706d1e0c749a
Pulga <juansie.96@gmail.com>
2020-04-29 20:43:36 -0300

    Add JQuery and Bootstrap libraries. Implement toggle sidebar functionality.

--- index.php
+++ index.php
@@ -4,18 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VideoTube</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/css/styles.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="./assets/js/commonActions.js"></script>
 </head>
 <body>
     <div id="pageContainer">
         <div id="mastHeadContainer">
-        testing
+            <button class="navShowHide">Click me</button>
         </div>
 
-        <div id="sideNavContainer">
+        <div id="sideNavContainer" style="display:none;">
         </div>
 
         <div id="mainSectionContainer">
+            <div id="mainContentContainer">
+                hello everyone
+            </div>
         </div>
     </div>
 </body>
